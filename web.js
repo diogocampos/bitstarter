5841207ba7a2c31b663766c66613f865826cef0d
Diogo Campos <diogocampos@me.com>
2013-07-08 17:13:55 -0300

    Reply with contents of index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var indexBuffer = fs.readFileSync('index.html');
+  var indexString = indexBuffer.toString();
+  response.send(indexString);
 });
 
 var port = process.env.PORT || 5000;
